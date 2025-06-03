e3096541da9122f7e39ea3ff32e52d3f66429d75
Damarrion Morgan Harper <dgodsonmo@gmail.com>
2025-06-03 16:00:05 -0700

    Update MonthlyChart.tsx

--- sAIve/src/components/MonthlyChart.tsx
+++ sAIve/src/components/MonthlyChart.tsx
@@ -22,18 +22,22 @@ import { useState } from "react";
 
 
 const chartConfig = {
-  expenses: {
+  TExpense: {
     label: "Expenses",
     color: "var(--chart-5)",
   },
-  income: {
+  TIncome: {
     label: "Income",
     color: "var(--chart-1)",
   },
-  savings: {
+  TSavings: {
     label: "Savings",
     color: "var(--chart-3)",
   },
+  net_worth: {
+      label: "Net Worth",
+      color: "var(--chart-2)",
+    },
 } satisfies ChartConfig
 
 
@@ -133,17 +137,53 @@ const getChartData = ({ assets }: { assets: any }) => {
         />
 
         <defs>
-              <linearGradient id="fillExpense" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="TExpense" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-expenses)"
+                  stopColor="var(--color-TExpense)"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-expenses)"
+                  stopColor="var(--color-TExpense)"
                   stopOpacity={0.1}
                 />
+              </linearGradient>
+              <linearGradient id="TIncome" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-TIncome )"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-TIncome )"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              <linearGradient id="TSavings" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-TSavings)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-TSavings)"
+                    stopOpacity={0.1}
+                  />
+              </linearGradient>
+              <linearGradient id="net_worth" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-net_worth)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-net_worth)"
+                    stopOpacity={0.1}
+                  />
               </linearGradient>
             </defs>
         
@@ -155,11 +195,11 @@ const getChartData = ({ assets }: { assets: any }) => {
           dataKey={datakey}
           animationEasing="ease"
           type="linear"
-          fill={`url(#fill${datakey})`}
+          fill={`url(#${datakey})`}
           fillOpacity={.4}
-          stroke={chartConfig[datakey]?.color || "var(--color-default)"}
+          stroke={`var(--color-${datakey})`}
           strokeWidth={2}
-          dot={{ fill: chartConfig[datakey]?.color || "var(--color-default)" }}
+          dot={{ fill: `var(--color-${datakey})` }}
           activeDot={{ r: 6 }}
         />
       </AreaChart>
